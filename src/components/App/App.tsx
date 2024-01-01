dd6837a0cb527f84e14de201fcb639de6eac0243
khilyao <sfwew19@gmail.com>
2024-01-01 23:58:37 +0200

    add homepage and moviespage views

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Navigation from "components/Navigation/Navigation";
+import HomePage from "views/HomePage/HomePage";
+import MoviesPage from "views/MoviesPage/MoviesPage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
